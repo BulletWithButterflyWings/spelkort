44b63d16b870da987f06d600e952b5c2a8bf46b4
Alfons Örtemyr <oral16@nti.umea>
2018-09-27 10:40:40 +0200

    Ändrat antal för nummer.

--- spelkort.py
+++ spelkort.py
@@ -1,5 +1,5 @@
 print("Hej och välkommen till mitt spelprogram!")
 print("Nu måste du gissa ett tal, och hoppas att det är rätt såklart! ")
 import random
-number=random.randint(1,4)
+number=random.randint(1,70)
 print(number)
